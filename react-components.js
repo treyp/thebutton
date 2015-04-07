2c1f2465d854e5079d1c256731dd1945ff8f3b8b
Trey Philips <treyjp@gmail.com>
2015-04-07 03:55:53 -0500

    initial work on completely new horizontal bar chart from scratch. still need to add back in an animated bar representing the current timer.

--- react-components.js
+++ react-components.js
@@ -1,4 +1,3 @@
-var clickTimes = [];
 var initialParticipants = 0;
 var currentParticipants = 0;
 var PureRenderMixin = React.addons.PureRenderMixin;
@@ -17,10 +16,10 @@ var TimerDisplay = React.createClass({
 var Tick = React.createClass({
     mixins: [PureRenderMixin],
     render: function() {
-        var evenOrOdd = this.props.count % 2 === 0 ? 'even' : 'odd';
+        var evenOrOdd = this.props.count % 2 === 0 ? "even" : "odd";
         return (
             <div className="tick">
-                <div className={'tock ' + evenOrOdd}></div>
+                <div className={"tock " + evenOrOdd}></div>
             </div>
         );
     }
@@ -29,10 +28,14 @@ var Tick = React.createClass({
 var StatsDisplay = React.createClass({
     mixins: [PureRenderMixin],
     render: function () {
+        var runningSince = "since " + this.props.started.format("LTS");
+        var runningDuration =
+            moment.duration(moment().diff(this.props.started)).humanize();
         return (
             <div className="stats">
                 <div>
-                    {"Started: "}<span>{this.props.started}</span>
+                    {"Running: "}
+                    <span title={runningSince}>{runningDuration}</span>
                 </div>
                 <div>
                     {"Lag: "}
@@ -47,14 +50,105 @@ var StatsDisplay = React.createClass({
     }
 });
 
+var Chart = React.createClass({
+    mixins: [PureRenderMixin],
+    componentDidMount: function () {
+        var data = this.props.times;
+        var chart = d3.select(React.findDOMNode(this));
+
+        this.xScale = d3.scale.linear()
+            .domain([0, 60])
+            .range([0, this.props.width]);
+
+        this.addBarsToSelection(
+            chart
+                .attr("width", this.props.width)
+                .selectAll("g")
+                .data(data).enter()
+        );
+        chart.attr("height", ((this.props.barHeight + 1) * data.length));
+    },
+    componentWillUpdate: function (props) {
+        var data = props.times;
+        var chart = d3.select(React.findDOMNode(this));
+        chart.attr("width", props.width);
+        this.xScale = this.xScale.range([0, props.width]);
+
+        var selection = chart
+            .selectAll("g").data(data);
+        this.updateBarsInSelection(selection);
+        this.addBarsToSelection(selection.enter());
+        selection.exit().remove();
+        chart.attr("height", ((props.barHeight + 1) * selection.size()));
+    },
+    addBarsToSelection: function (selection) {
+        selection = selection
+            .append("g");
+        selection.append("rect");
+        selection.append("text");
+        return this.updateBarsInSelection(selection);
+    },
+    updateBarsInSelection: function (selection) {
+        var self = this;
+
+        selection
+            .attr("transform", function (d, i) {
+                return "translate(0," +
+                    (selection.size() - i - 1) * (self.props.barHeight + 1) +
+                    ")";
+            });
+        selection.select("rect")
+            .attr("width", function (d) {
+                return Math.max(1, self.xScale(60 - d));
+            })
+            .attr("height", this.props.barHeight)
+            .attr("class", function (d) { return self.flairClass(d); });
+        selection.select("text")
+            .attr("x", function (d) {
+                var barWidth = Math.max(1, self.xScale(60 - d));
+                return barWidth < 20 ? barWidth + 3 : barWidth - 3;
+            })
+            .classed("outside", function (d) {
+                return self.xScale(60 - d) < 20;
+            })
+            .attr("y", this.props.barHeight / 2)
+            .attr("dy", ".35em")
+            .text(function (d) { return d; });
+        return selection;
+    },
+    flairClass: function (seconds) {
+        if (seconds > 51) {
+            return "flair-press-6";
+        }
+        if (seconds > 41) {
+            return "flair-press-5";
+        }
+        if (seconds > 31) {
+            return "flair-press-4";
+        }
+        if (seconds > 21) {
+            return "flair-press-3";
+        }
+        if (seconds > 11) {
+            return "flair-press-2";
+        }
+        return "flair-press-1";
+    },
+    render: function () {
+        return <svg className="chart"></svg>;
+    }
+});
+
 var ButtonMonitor = React.createClass({
     getInitialState: function () {
         return {
-            started: moment().format("YYYY-MM-DD HH:mm:ss"),
+            started: moment(),
             lag: 0,
             participants: 0,
             secondsRemaining: 60.0,
-            ticks: 0
+            ticks: 0,
+            times: [], // [60,59,18,60,59,25,60,8,0,3,45,35,60,55],
+            chartWidth: 0
         };
     },
     tick: function () {
@@ -72,18 +166,31 @@ var ButtonMonitor = React.createClass({
     syncTimer: function (secondsRemaining) {
         this.setState({secondsRemaining: secondsRemaining});
     },
+    addTime: function (seconds) {
+        this.setState({times: this.state.times.concat(seconds)});
+    },
+    windowResized: function () {
+        this.setState({chartWidth: React.findDOMNode(this).offsetWidth});
+    },
     componentDidMount: function () {
         this.interval = setInterval(this.tick, 100);
+
+        // thanks to React's autobinding, no need to worry about 'this' in the
+        // handler call
+        window.addEventListener("resize", this.windowResized);
+        this.windowResized();
+
         var previousSecondsLeft;
         var previousParticipants;
         var self = this;
         var socket = new WebSocket(
             "wss://wss.redditmedia.com/thebutton?h=" +
-            "af117cac0144724620b827801e9c18e4951492df&e=1428374000"
+            "1d15884a584a15b84dcb15561824698e9f43cf9f&e=1428466168"
         );
         socket.onmessage = function (event) {
-            /* jshint camelcase: false */
+            /* jshint camelcase: false, maxstatements: 20 */
             // disabling camelcase since reddit uses underscore style here
+            // also bumping maxstatements until i have a chance to refactor
             /*
             sample tick data:
             {
@@ -116,23 +223,23 @@ var ButtonMonitor = React.createClass({
             }
             self.updateParticipants(currentParticipants);
 
-            if (previousParticipants && previousParticipants < currentParticipants) {
+            if (previousParticipants &&
+                previousParticipants < currentParticipants) {
                 // time to create new chart bars
-                clickTimes.push(previousSecondsLeft);
-                // fill in the gaps when more than one person clicks between ticks
+                self.addTime(previousSecondsLeft);
+                // fill in the gaps when more than one person clicks between
+                // ticks
                 var barCountOffset =
                     (currentParticipants - initialParticipants) -
-                    clickTimes.length;
+                    self.state.times.length;
                 if (barCountOffset > 0) {
                     while (barCountOffset > 0) {
-                        clickTimes.push(60.0);
+                        self.addTime(60.0);
                         barCountOffset = barCountOffset - 1;
                     }
                 }
-                Chart.render(clickTimes);
             }
 
-            Timer.sync(tick.seconds_left);
             self.syncTimer(tick.seconds_left);
 
             previousSecondsLeft = tick.seconds_left;
@@ -141,6 +248,7 @@ var ButtonMonitor = React.createClass({
     },
     componentWillUnmount: function () {
         clearInterval(this.interval);
+        window.removeEventListener("resize", this.windowResized);
     },
     render: function () {
         return (
@@ -152,6 +260,10 @@ var ButtonMonitor = React.createClass({
                     started={this.state.started}
                     lag={this.state.lag}
                     participants={this.state.participants} />
+                <Chart
+                    times={this.state.times}
+                    barHeight={20}
+                    width={this.state.chartWidth} />
             </div>
         );
     }
