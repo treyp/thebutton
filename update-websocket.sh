d5ae8ab617a6e46bd9f9e4e2928ad86163bfd05c
Trey Philips <treyjp@gmail.com>
2015-04-14 18:48:30 -0500

    adding output messages to update-websocket.sh

--- update-websocket.sh
+++ update-websocket.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+set -e
+msg() {
+    echo "[1;34m> [1;32m$@[0m"
+}
+
+msg "Grabbing latest websocket URL from Reddit..."
 curl -s "http://www.reddit.com/r/thebutton" | perl -ne '/wss:\/\/[^"]+/ && print $&' > websocket-url.txt
 git add websocket-url.txt
 git commit -m "websocket url update"
+msg "Updated websocket-url.txt."
